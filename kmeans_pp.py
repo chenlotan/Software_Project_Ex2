895312f22575547e25fe1659a198688c149e2b51
chenlotan <chenlotan@mail.tau.ac.il>
2021-12-22 11:41:51 +0200

    python fixed. c with marked lines

--- kmeans_pp.py
+++ kmeans_pp.py
@@ -9,41 +9,49 @@ def validate(condition):
         exit(1)
 
 
-def join_inputs(file1, file2):  #TODO
-    pass
+def check_if_float(num):
+    try:
+        float(num)
+    except ValueError:
+        print('Invalid Input!')
+        exit(1)
 
 
-# Open the file and create array of vectors
-def read_file(file_name):
-    f = open(file_name, "r")
-    vectors = []
-    for line in f:
-        if line != "\n":
-            numbers = line.split(',')
-            for i in range(len(numbers)):
-                numbers[i] = float("%0.4f" % float(numbers[i]))
-            vectors.append(numbers)
-    return vectors
+def read_file_to_df(file):
+    if file.split(".")[-1] == "txt":
+        return pd.read_csv(file, sep=",", header=None)
+    else:
+        return pd.read_csv(file)
 
 
-def initialize_centroids(vectors, k):  # TODO
-    mu = []
+def initialize_centroids(vectors, k):
+    mu = np.zeros((k, vectors.shape[1]))
+    mu_index = np.zeros(k)
     np.random.seed(0)
-    mu.append(np.random.choice(vectors))
-    for i in range(k-1):
-        pass
-    return mu
+    N = vectors.shape[0]
+    mu_index[0] = np.random.choice(N)
+    mu[0] = vectors[(int(mu_index[0]))]
+    for i in range(1, k):
+        D = np.apply_along_axis(lambda vector: min(np.linalg.norm(vector-mu[:i], axis=1)**2), axis=1, arr=vectors)
+        p = D/np.sum(D)
+        mu_index[i] = np.random.choice(N, p=p)
+        mu[i] = vectors[int(mu_index[i])]
+    return mu_index.astype(int)
 
 
 args = sys.argv[1:]
-validate(3 <= len(args) <= 4)
-k, max_iter, epsilon, input1_filename, input2_filename = args[0], args[1] if len(args) == 4 else 300, args[-3], args[-2], args[-1]
+validate(4 <= len(args) <= 5)
+k, max_iter, epsilon, input1_filename, input2_filename = args[0], args[1] if len(args) == 5 else 300, args[-3], args[-2], args[-1]
 validate(args[0].isdigit())
-if len(args) == 4:
+check_if_float(args[-3])
+validate(float(args[-3]) >= 0)
+if len(args) == 5:
     validate(max_iter.isdigit())
     max_iter = int(max_iter)
 k = int(k)
-input_filename = join_inputs(input1_filename, input2_filename)  #TODO
-vectors = read_file(input_filename)
-d = len(vectors[0])
-mu = initialize_centroids(vectors, k)
+epsilon = float(epsilon)
+df1 = read_file_to_df(input1_filename)
+df2 = read_file_to_df(input2_filename)
+vectors = pd.merge(df1, df2, on=0).sort_values(by=0).iloc[:, 1:].to_numpy()
+print(initialize_centroids(vectors, k))
+
